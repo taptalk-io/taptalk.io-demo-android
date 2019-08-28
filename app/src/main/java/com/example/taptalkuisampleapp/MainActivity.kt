ccf03fb0ac53891524dbf0569aa0f96ddc2c4b3f
Jefry Lorentono <jefry@moselo.com>
2019-08-28 16:21:19 +0700

    Upgrade Taptalk.io SDK

--- app/src/main/java/com/example/taptalkuisampleapp/MainActivity.kt
+++ app/src/main/java/com/example/taptalkuisampleapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.taptalkuisampleapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import io.taptalk.TapTalk.Helper.TapTalk
+import io.taptalk.TapTalk.Manager.TAPDataManager
+import io.taptalk.TapTalk.View.Activity.TAPLoginActivity
+import io.taptalk.TapTalk.View.Activity.TAPRoomListActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TapTalk.checkActiveUserToShowPage(this)
+
+        val intent: Intent
+        if (TAPDataManager.getInstance().checkAccessTokenAvailable()!!) {
+            intent = Intent(this@MainActivity, TAPRoomListActivity::class.java)
+        } else {
+            intent = Intent(this@MainActivity, TAPLoginActivity::class.java)
+        }
+        startActivity(intent)
+        finish()
+
     }
 }
--- app/src/main/java/com/example/taptalkuisampleapp/SampleApplication.java
+++ app/src/main/java/com/example/taptalkuisampleapp/SampleApplication.java
@@ -2,8 +2,9 @@ package com.example.taptalkuisampleapp;
 
 import android.app.Application;
 import android.util.Log;
+
 import io.taptalk.TapTalk.Helper.TapTalk;
-import io.taptalk.TapTalk.Listener.TAPListener;
+import io.taptalk.TapTalk.Listener.TapListener;
 import io.taptalk.TapTalk.Model.TAPMessageModel;
 
 public class SampleApplication extends Application {
@@ -13,32 +14,32 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        TapTalk.setTapTalkEnvironmentDevelopment();
-        TapTalk.init(
-                this,
-                "d1e5dfe23d1e00bf54bc2316f",
+        TapTalk.init(this, "d1e5dfe23d1e00bf54bc2316f",
                 "NTQzMTBjZDI5YWNjNTEuMS4x/ZDY4MTg3Yjg/OTA0MTQwNDFhMDYw/MGI0YjA5NTJjM2Fh",
-                R.mipmap.ic_launcher,
-                "TapTalk UI Sample Application",
-                TapTalk.TapTalkImplementationType.TapTalkImplentationTypeUI,
-                tapListener
-        );
+                //R.mipmap.ic_launcher, getResources().getString(R.string.tap_app_name),
+                R.mipmap.ic_launcher, "TapTalk Dev Sample App", "engine-dev.taptalk.io",
+                TapTalk.TapTalkImplementationType.TapTalkImplementationTypeUI,
+                tapListener);
+        TapTalk.initializeGooglePlacesApiKey("AIzaSyA1kCb7yq2shvC3BnzriJLcTfzQdmzSnPA");
     }
 
-    private TAPListener tapListener = new TAPListener() {
+    private TapListener tapListener = new TapListener() {
         @Override
-        public void onRefreshAuthTicket() {
-            Log.e(TAG, "onRefreshAuthTicket: " );
+        public void onTapTalkRefreshTokenExpired() {
+            super.onTapTalkRefreshTokenExpired();
+            Log.e(TAG, "onTapTalkRefreshTokenExpired: ");
         }
 
         @Override
-        public void onTapTalkUnreadChatRoomBadgeCountUpdated(int unreadCount) {
+        public void onTapTalkUnreadChatRoomBadgeCountUpdated(int i) {
+            super.onTapTalkUnreadChatRoomBadgeCountUpdated(i);
             Log.e(TAG, "onTapTalkUnreadChatRoomBadgeCountUpdated: ");
         }
 
         @Override
-        public void onNotificationReceived(TAPMessageModel messageModel) {
-            Log.e(TAG, "onNotificationReceived: " + messageModel.getUser().getName() + ": " + messageModel.getBody());
+        public void onNotificationReceived(TAPMessageModel tapMessageModel) {
+            super.onNotificationReceived(tapMessageModel);
+            Log.e(TAG, "onNotificationReceived: ");
         }
     };
 }
